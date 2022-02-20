68273f97e748b6e7089ebe92600b9f1a96f07d99
TijanAyo <ayomidetijani6@gmail.com>
2022-02-20 09:29:29 +0100

    Added middleware to help with authorization

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const {getStatus, getGoals, createGoal, updateGoal, deleteGoal} = require('../controller/goalcontroller')
+const { protect } = require('../middleware/authMiddleware')
 
 // @desc: Checking status of API
 router.get('/api/status', getStatus);
 
 // @desc: List out all goals
-router.get('/api/goals', getGoals);
+router.get('/api/goals', protect, getGoals);
 
 // @desc: Create goal
-router.post('/api/goals', createGoal);
+router.post('/api/goals', protect, createGoal);
 
 // @desc: Update goal
-router.put('/api/goals/:id', updateGoal);
+router.put('/api/goals/:id', protect, updateGoal);
 
 // @desc: Delete goal
-router.delete('/api/goals/:id', deleteGoal);
+router.delete('/api/goals/:id', protect, deleteGoal);
 
 module.exports = router
